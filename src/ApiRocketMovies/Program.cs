eef66a0bb6fc60bafc98842bf211582624e01aec
ronaldo <ronaldo.dsantos@gmail.com>
2025-02-10 22:16:30 -0300

    add authentication jwt bearer

--- src/ApiRocketMovies/Program.cs
+++ src/ApiRocketMovies/Program.cs
@@ -1,6 +1,10 @@
 using ApiRocketMovies.Data;
+using ApiRocketMovies.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +24,30 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>();
+
+var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings"); 
+builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+var JwtSettings = JwtSettingsSection.Get<JwtSettings>(); 
+var key = Encoding.ASCII.GetBytes(JwtSettings.Segredo);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true; 
+    options.SaveToken = true; 
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key), 
+        ValidateIssuer = true, 
+        ValidateAudience = true, 
+        ValidAudience = JwtSettings.Audiencia, 
+        ValidIssuer = JwtSettings.Emissor 
+    };
+});
 
 var app = builder.Build();
 
