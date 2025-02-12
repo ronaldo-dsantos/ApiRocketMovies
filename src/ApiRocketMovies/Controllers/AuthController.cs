ae4cf044a9a371a125d8c748a107436414803266
ronaldo <ronaldo.dsantos@gmail.com>
2025-02-12 00:23:36 -0300

    edit method update users controllers

--- src/ApiRocketMovies/Controllers/AuthController.cs
+++ src/ApiRocketMovies/Controllers/AuthController.cs
@@ -2,7 +2,6 @@
 using ApiRocketMovies.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -11,7 +10,7 @@ using System.Text;
 namespace ApiFuncional.Controllers
 {
     [ApiController]
-    [Route("api/conta")]
+    [Route("api/sessions")]
     public class AuthController : ControllerBase
     {
         private readonly SignInManager<User> _signInManager;
@@ -27,39 +26,7 @@ namespace ApiFuncional.Controllers
             _jwtSettings = jwtSettings.Value;
         }
 
-        [HttpPost("registrar")]
-        public async Task<ActionResult> Registrar(CreateUserDto createUserDto)
-        {
-            if (!ModelState.IsValid) return ValidationProblem(ModelState);
-
-            var checkEmailExists = await _userManager.Users.AnyAsync(u => u.Email == createUserDto.Email);
-            if (checkEmailExists)
-            {
-                return BadRequest(new { Message = "Este e-mail já está em uso." });
-            }
-
-            var user = new User
-            {
-                Name = createUserDto.Name,
-                UserName = createUserDto.Email,
-                Email = createUserDto.Email,
-                EmailConfirmed = true,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            var result = await _userManager.CreateAsync(user, createUserDto.Password);
-
-            if (result.Succeeded)
-            {
-                await _signInManager.SignInAsync(user, false);
-                return Ok(GerarJwt());
-            }
-                        
-            return BadRequest(new { Message = "Falha ao registrar o usuário" });
-        }
-
-        [HttpPost("login")]
+        [HttpPost]
         public async Task<ActionResult> Login(LoginUserDto loginUser)
         {
             if (!ModelState.IsValid) return ValidationProblem(ModelState);
--- src/ApiRocketMovies/Controllers/UsersController.cs
+++ src/ApiRocketMovies/Controllers/UsersController.cs
@@ -1,8 +1,9 @@
-﻿using ApiRocketMovies.Data;
-using ApiRocketMovies.DTOs;
+﻿using ApiRocketMovies.DTOs;
 using ApiRocketMovies.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace ApiRocketMovies.Controllers
 {
@@ -10,48 +11,51 @@ namespace ApiRocketMovies.Controllers
     [Route("api/users")]
     public class UsersController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly JwtSettings _jwtSettings;
 
-        public UsersController(AppDbContext context)
+        public UsersController(SignInManager<User> signInManager,
+                              UserManager<User> userManager,
+                              IOptions<JwtSettings> jwtSettings)
         {
-            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
         }
 
         [HttpPost]
         public async Task<ActionResult> Create(CreateUserDto createUserDto)
         {
-            if (!ModelState.IsValid)
-            {
-                return ValidationProblem(ModelState);
-            }
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
 
-            if (createUserDto == null)
-            {
-                return BadRequest(new { Message = "Dados inválidos." });
-            }
-
-            var checkEmailExists = await _context.Users.AnyAsync(u => u.Email == createUserDto.Email);
+            var checkEmailExists = await _userManager.Users.AnyAsync(u => u.Email == createUserDto.Email);
             if (checkEmailExists)
-            {           
+            {
                 return BadRequest(new { Message = "Este e-mail já está em uso." });
             }
-
-            //var passwordHash = BCrypt.Net.BCrypt.HashPassword(createUserDto.Password);
 
             var user = new User
             {
                 Name = createUserDto.Name,
+                UserName = createUserDto.Email,
                 Email = createUserDto.Email,
-                PasswordHash = createUserDto.Password,
+                EmailConfirmed = true,
                 CreatedAt = DateTime.Now,
                 UpdatedAt = DateTime.Now
             };
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, createUserDto.Password);
 
-            return Created();
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Created();
+            }
+
+            return BadRequest(new { Message = "Falha ao registrar o usuário" });
         }
+
 
         [HttpPut("{id}")]
         public async Task<ActionResult> Update(string id, UpdateUserDto updateUserDto)
@@ -59,27 +63,25 @@ namespace ApiRocketMovies.Controllers
             if (!ModelState.IsValid)
             {
                 return ValidationProblem(ModelState);
-            }
+            }            
 
-            if (updateUserDto == null)
-            {             
-                return BadRequest(new { Message = "Dados inválidos." });
-            }
+            var user = await _userManager.FindByIdAsync(id);
 
-            var user = await _context.Users.FindAsync(id);
             if (user == null)
-            {                
-                return BadRequest(new { Message = "Usuário não encontrado." });
+            {
+                return NotFound(new { Message = "Usuário não encontrado." });
             }
 
-            var emailExists = await _context.Users.FirstOrDefaultAsync(u => u.Email == updateUserDto.Email);
-            if (emailExists != null && emailExists.Id != user.Id)
+            var emailExists = await _userManager.Users.AnyAsync(u => u.Email == updateUserDto.Email && u.Id != id);
+
+            if (emailExists)
             {
-                return BadRequest(new { Message = "Este e-mail já está em uso." });
+                return BadRequest(new { Message = "Este e-mail já está em uso por outro usuário." });
             }
 
             user.Name = updateUserDto.Name ?? user.Name;
             user.Email = updateUserDto.Email ?? user.Email;
+            user.UserName = updateUserDto.Email ?? user.UserName; 
 
             if (!string.IsNullOrEmpty(updateUserDto.Password))
             {
@@ -88,21 +90,29 @@ namespace ApiRocketMovies.Controllers
                     return BadRequest(new { Message = "Para alterar a senha, informe a senha antiga." });
                 }
 
-                var isOldPasswordValid = BCrypt.Net.BCrypt.Verify(updateUserDto.OldPassword, user.PasswordHash);
-                if (!isOldPasswordValid)
+                var passwordValid = await _userManager.CheckPasswordAsync(user, updateUserDto.OldPassword);
+                if (!passwordValid)
                 {
                     return BadRequest(new { Message = "Senha antiga incorreta." });
                 }
 
-                user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(updateUserDto.Password);
+                var passwordChangeResult = await _userManager.ChangePasswordAsync(user, updateUserDto.OldPassword, updateUserDto.Password);
+                if (!passwordChangeResult.Succeeded)
+                {
+                    return BadRequest(new { Message = "Falha ao atualizar a senha." });
+                }
             }
 
             user.UpdatedAt = DateTime.Now;
+            
+         
+            var updateResult = await _userManager.UpdateAsync(user);
+            if (!updateResult.Succeeded)
+            {
+                return BadRequest(new { Message = "Falha ao atualizar os dados do usuário." });
+            }
 
-            _context.Users.Update(user);
-            await _context.SaveChangesAsync();
-
-            return Ok();
+            return Ok(new { Message = "Usuário atualizado com sucesso." });
         }
     }
 }
